56b50bde19a28c15e9bf359bc7c97df4c80e8f71
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-04-27 21:24:27 +0000

    Add rate limiting and fingerprinting middleware

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const __dirname = path.join(__filename)
 import morgan from 'morgan';
 import Database from 'better-sqlite3';
 import cors from 'cors';
+import rateLimit from 'express-rate-limit';
+import cookieParser from 'cookie-parser';
+import crypto from 'crypto';
 import {
     ipRoute,
     apiWorking,
@@ -27,16 +30,75 @@ app.use(express.json());
 app.use(express.urlencoded({ extended : true}));
 app.use(express.static(path.join(__dirname,"Front_End")))
 app.use(express.static(path.join(__dirname,"Front_End","CSS")));
-app.use(morgan("dev"))
+app.use(morgan("dev"));
+app.use(cookieParser());
 app.set("trust proxy",1);
-app.set("json spaces",1.4)
+app.set("json spaces",2)
 const PORT = 53727;
 
+
+
+
+// Device if Track;
+
+
+
+//Rate limiting too many requests 
+  app.use(rateLimit({
+      windowMs : 1000 * 60,
+      max : 60,
+      message : (["Too many request within the range of 1(one) minute. Please try again after a minutes"])
+})
+  
+  )
+
+
+function fingerprint(req,res,next){
+
+  const raw =
+    (req.headers["user-agent"] || "") +
+    (req.headers["accept-language"] || "") +
+    (req.headers["accept-encoding"] || "") +
+    (req.headers["sec-ch-ua"] || "") +
+    (req.headers["sec-ch-ua-platform"] || "")
+  +(req.headers["sec-ch-ua-mobile"] || "");
+
+  req.fingerprint = crypto
+    .createHash("sha256")
+    .update(raw)
+    .digest("hex");
+  next();
+}
+
+app.use(fingerprint);
+
+
+//blocked ips
+const blockedDevices = ["6b2b8376ed9b8ac46a42b7d5d9481982d079cc5f0993f518e6090815f6f4d1bf"];
+
+
+function checkBlocked(req,res,next){
+   
+    if(blockedDevices.includes(req.fingerprint)){
+               console.log(`\n\n\x1b[1;36m A user with deviceId\x1b[0m \x1b[3;32m${req.fingerprint}\x1b[0m \x1b[1;36mtried accessing cypherApi but has been blocked due to suspicious activities\x1b[0m`)
+ return res.status(403).json({ 
+     Developer : "cyphers",
+     statusCode : 429,
+     message : " you have been blocked due to suspicion",
+     Option : " please contact support @ cybercyphers2008@gmail.com"
+     
+    
+     
+ });
+ 
+ }
+   next()
+}
 //  only packages import execution ends here.
 //-------------------------------------------
 
 // other files actions or import actions
-
+app.use(checkBlocked)
 
 
 ipRoute(app)
@@ -44,6 +106,10 @@ apiWorking(app)
 Ping(app)
 PreviewImg(app)
 Email(app)
+
+app.get("/cyphers/try",(req,res)=>{
+ res.json({ message : req.fingerprint })
+})
 //ither file actions ends here
  
 //-------------------------------------------
