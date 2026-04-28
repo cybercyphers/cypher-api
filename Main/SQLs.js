6a1bc09a448c4c81885c2efbfb171ecf78592c6d
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-04-28 21:00:33 +0000

    Add users table and insert function to SQLs.js

--- Main/SQLs.js
+++ Main/SQLs.js
@@ -1,7 +1,14 @@
 import Database from 'better-sqlite3';
 const db = new Database("./DataBases/IDS.db");
 
-db.exec("CREATE TABLE IF NOT EXISTS cyphers(name TEXT)");
+db.exec("CREATE TABLE IF NOT EXISTS users(id VARCHAR(10) UNIQUE, name TEXT, online BOOLEAN DEFAULT false, joined_at DATETIME DEFAULT CURRENT_TIMESTAMP)");
+
+const newInsert = db.exec("INSERT INTO users(id,name,online) VALUES(?,?,?)");
 
 
-export { db };
+
+export  { 
+    db,
+    newInsert
+}
+    
