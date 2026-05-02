4a5ed18cb99b14ef2e1e782187e2992df94f7653
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-05-02 20:48:38 +0000

    Refactor users table and insert logic in SQLs.js
    Updated users table schema and modified insert logic to use a transaction.

--- Main/SQLs.js
+++ Main/SQLs.js
@@ -1,9 +1,20 @@
 import Database from 'better-sqlite3';
 const db = new Database("./DataBases/IDS.db");
 
-db.exec("CREATE TABLE IF NOT EXISTS users(id VARCHAR(10) UNIQUE, name TEXT, online BOOLEAN DEFAULT false, joined_at DATETIME DEFAULT CURRENT_TIMESTAMP)");
+db.exec("CREATE TABLE IF NOT EXISTS users(id VARCHAR(10) UNIQUE, username TEXT UNIQUE , online BOOLEAN DEFAULT false,role TEXT DEFAULT user, joined_at DATETIME DEFAULT CURRENT_TIMESTAMP)");
 
-const newInsert = db.exec("INSERT INTO users(id,name,online) VALUES(?,?,?)");
+const newInsert = db.transaction((id,username)=>{
+  const smtp = db.prepare("INSERT OR IGNORE INTO users(id,username) VALUES(?,?)")
+  smtp.run(id,username);
+});
+
+
+setTimeout(()=>{
+const d = db.prepare("SELECT * FROM users").all()
+console.log(d);
+},15
+)
+
 
 
 
