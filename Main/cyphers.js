e0e588c3b75ac748c059de219a6b96a749ab923e
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-05-08 20:57:49 +0000

    Refactor database handling and user authentication

--- Main/cyphers.js
+++ Main/cyphers.js
@@ -8,20 +8,59 @@ const __dirname = path.dirname(__filename);
 import os from 'os';
 import rateLimit from 'express-rate-limit';
 import Database from 'better-sqlite3';
-import  { 
-    db, 
-    newInsert
-} from './SQLs.js';
+import bcrypt from 'bcrypt';
+import  db,{    
+    newInsert,
+    sdb,
+    signUpsInsert,
+    dbUserCheck,
+    isAlreadyLoggedInToggle,
+    isAlreadyLoggedInToggleFalse,
+    usernameResetSelect,
+    userPasswordReset
+} from './SQL.js';
 import crypto from 'crypto';
+
+import { saveErrorLog } from '../index.js';
 
 const ID = crypto.randomBytes(3).toString("hex")
 //-----Ã¢â‚¬â€-------------------------------------
+
+db.pragma("journal_mode=WAL");
+db.pragma("synchronous=FULL");
+sdb.pragma("journal_mode=WAL");
+sdb.pragma("synchronous=FULL");
+const sdbIntegrityCheck = sdb.pragma("integrity_check",{ simple:true });
+const dbIntegrityCheck = db.pragma("integrity_check",{ simple : true });
+db.pragma("wal_checkpoint");
+sdb.pragma("wal_checkpoint")
+function databaseIntegrityCheck(){
+  if(sdbIntegrityCheck !== "ok" && dbIntegrityCheck !== "ok" ){
+   console.log("\n\x1b[1;31mDatabase might have been corrupted please fix\x1b[0m\n");
+ }else{
+    console.log("\n\x1b[1;32mDatabase is operational and secure, initializing next code.....\x1b[0m\n")
+ }
+}
+databaseIntegrityCheck()
+
 function logDir(){
  const filePath = path.join(__dirname,"../maintainance","config.json");
     const filePathRead = fs.readFileSync(filePath,"utf8");
     const logdir = JSON.parse(filePathRead).DIRS;
     return logdir;
 }
+
+//save info
+
+function saveUserInfo(user,info){
+  const dirPath = path.join(os.homedir(),"user_info");
+    const userFilePath = path.join(dirPath,user+"_user"+".txt")
+ if(!fs.existsSync(dirPath)){
+     fs.mkdirSync(dirPath,{ recursive :  true })
+ }
+   fs.appendFileSync(userFilePath,info+"\n")
+}
+
 
 
 
@@ -81,7 +120,7 @@ const sendSuccess =(res,statusCode,message)=>{
 
 // function to send error if false 
 function sendError(res,statusCode,message){
- return res.status(500).json({
+ return res.status(statusCode).json({
      Developer : "cyphers",
      success : false,
      Date : date(),
@@ -90,18 +129,8 @@ function sendError(res,statusCode,message){
 })
 }
 
-// system Error save
-function saveErrorLog(){
-    try{
-    const homeDir = os.homedir(); 
- const errorPath = path.join(homeDir,"serverErrors");
-   const errorFile = path.join(errorPath,date()+".txt")
- if(!fs.existsSync(errorPath)) {
-     fs.mkdirSync(errorPath, {recursive : true})
-                               }
-    }catch(err){
-console.log("could not save error to file", err)}
-}
+
+
 
 //--------------------------â€¢â€¢--------------
 // preview image
@@ -132,12 +161,124 @@ const ipRoute =(app) =>{
 //-------------------------------------------
 const apiWorking = (app)=>{
  app.get("/cyphers/home_page",(req,res,next)=>{
+if(!req.session.user){
+   return res.status(403).send(`<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>âš ï¸ 403 Unauthenticated</title>
 
-    return res.status(200).sendFile(path.join(__dirname ,"../Front_End","home_page","index.html"))
+  <style>
+    *{
+      box-sizing:border-box;
+      margin:0;
+      padding:0;
+      font-family: Arial, sans-serif;
+    }
+
+    body{
+      min-height:100vh;
+      display:flex;
+      justify-content:center;
+      align-items:center;
+      background: linear-gradient(120deg, #000000, #0b0f14, #000000);
+      color:white;
+      flex-direction : column;
+margin:0;
+background-color:black;
+overscroll-behaviour-x : contain;
+    }
+
+    .card{
+      height:auto;
+      width:100%;
+      max-width:800px;
+      padding:50px 40px;
+      text-align:center;
+      border-radius:25px;
+       background:rgba(255,255,255,0.08);
+      backdrop-filter:blur(15px);
+      box-shadow:0 10px 30px rgba(0,0,0,.35);
+      border:1px solid rgba(255,255,255,.15);
+      margin: 100px;
+    }
+
+    .code{
+      font-size:90px;
+      font-weight:900;
+      color:#f87171;
+      margin-bottom:10px;
+      text-shadow:0 0 20px rgba(248,113,113,.4);
+    }
+
+    h1{
+      margin-bottom:12px;
+      font-size:35px;
+      color : red;
+    }
+
+    p{
+      color:#cbd5e1;
+      line-height:1.6;
+      margin-bottom:30px;
+      font-size:15px;
+    }
+
+    .btn{
+      display:inline-block;
+      text-decoration:none;
+      padding:14px 28px;
+      border-radius:15px;
+      background:#22c55e;
+      color:white;
+      font-weight:700;
+      transition:.25s;
+    }
+
+    .btn:hover{
+      transform:translateY(-2px);
+      box-shadow:0 8px 20px rgba(34,197,94,.35);
+    }
+main{
+ flex:1;
+display:flex;
+justify-content:center;
+align-items:center;
+}
+
+   
+  </style>
+</head>
+<body>
+<main>
+  <div class="card">
+ 
+    <div class="code">403</div>
+    <h1>Unauthenticated</h1>
+    <p>
+      You session has expired.please login to be verified
+    </p>
+
+    <a href="/cyphers/login" class="btn">Go to Login</a>
+  </div>
+</main><br/><br/>
+<footer style="display:flex; justify-content:center; align-items:center; width:100%; bottom:0;border-top: solid 0.7px burlywood;">
+<h6 style="margin:10px;">&copy; 2026 Cyber Cyphers. All rights reserved</h6>
+</footer>
+</body>
+</html>`)
+}
+     
+return res.status(200).sendFile(path.join(__dirname ,"../Front_End","home_page","index.html"))
+
+
   }
  
 )
          }
+
+
 
 //ping route
 //------------/------------/-------------/---
@@ -196,24 +337,11 @@ const Ping = (app)=>{
 //email part startup 
 
 const Email =(app)=>{
-  app.post("/cyphers/StartEmail",(req,res,next)=>{
-   const { startEmail, startPin } = req.body;
-      if(req.session.user){
-          next()
-      }else if(!startEmail){
-    return sendError(res,400,"Email address not specified");
-  }
-  newInsert(ID,startEmail)
-      if(startEmail === "CYPHERS"){
-     setTimeout(()=>{
- db.prepare("UPDATE users SET role=? WHERE username = ?").run('Admin Dev','CYPHERS')
-},10)
-      }
-    req.session.user = startEmail;   
-      res.status(200).json({message:"continue"})
-      
-     
-})
+    try{
+  
+    }catch(err){
+ console.log(err)
+     }
 }
 
 //github info and download
@@ -263,7 +391,7 @@ const totalRequests=(app)=>{
 app.use((req,res,next)=>{
   if(req.path !== "/cyphers/totalRequests"){
   totalReqs++
-      console.log(totalReqs++)
+     
   }
     next()
 })
@@ -284,6 +412,173 @@ const About = (app)=>{
    )
 }
 
+//signup route
+const signUpPOST =(app)=>{
+   
+    app.post("/cyphers/sign_up",async(req,res)=>{
+        try{
+ const { username, password } = req.body || {};
+    if(!username){
+     return sendError(res,400,"Please give a unique username");
+ };
+        if(!password){
+      return sendError(res,400,"please enter a password to continue");
+};
+            if(password.length <= 5 || password.length > 15){
+     return  sendError(res,400,"Password should be at least six characters ")
+}
+        const hashedPass = await bcrypt.hash(password,10)
+   const result =  signUpsInsert(username,hashedPass);
+            
+            
+          if(!result || result.changes <= 0){
+        return sendError(res,409,"user already exists")
+}
+   
+        sendSuccess(res,200,"signup was successfull");
+            console.log(`\x1b[36mA new user signed Up. Identifier ${username} at ${new Date().toISOString()}`)
+           
+    }catch(err){
+       console.log(err)
+       
+     }
+   })
+};
+
+
+
+
+//signup route
+const signUpGET = (app)=>{
+  app.get("/cyphers/sign_up",(req,res)=>{
+    if(!req.session.user){
+      return res.status(200).sendFile(path.join(__dirname,"../Front_End","home_page","signup.html")
+)
+    }
+      return res.redirect("/cyphers/home_page")
+    }
+           )
+}
+    
+
+const logInPOST = (app)=>{
+  app.post("/cyphers/login",async(req,res)=>{
+      try{
+    let { username, password } = req.body;
+      if(!username || !password){
+   return sendError(res,400,"All fields required");
+ }
+      
+      const userCheck = dbUserCheck.get(username);
+      if(!userCheck){
+          return sendError(res,404,"user not found")
+      }
+          
+     const userPassCheck = userCheck.signup_password;
+     
+   
+    
+    
+    
+     const passwordCompare = await bcrypt.compare(password,userPassCheck);
+          if(!passwordCompare){
+          return sendError(res,400,"wrong password, please try again ")
+              
+       }   
+          // check wheather already logged in on other device and set status
+          
+      const isAlreadyLoggedInCheck = userCheck.isAlreadyLoggedIn;
+          if(isAlreadyLoggedInCheck===true || isAlreadyLoggedInCheck === 1){
+    return sendError(res,res.statusCode,"Please logout from other device to be able to login here")
+}
+          
+         isAlreadyLoggedInToggle.run(username);
+          
+          req.session.user=username;
+          const finalInfo = `${username} logged in at ${new Date().toISOString()}, saving session....`;
+       //save info for each user
+          saveUserInfo(req.session.user,finalInfo)
+   return  sendSuccess(res,200,"Login successful");
+       
+               
+     }catch(err){
+       return res.send("An Error Occured")
+         saveErrorLog(err)
+        
+     };
+ });
+};
+
+const logInGET = (app,brutePrevent)=>{
+  app.get( "/cyphers/login",(req,res)=>{
+return res.status(200).sendFile(path.join(__dirname,"../Front_End","home_page","login.html"))
+  });
+};
+
+const logOut = (app)=>{
+    
+  app.get("/cyphers/logout",(req,res)=>{
+      try{  
+        isAlreadyLoggedInToggleFalse.run(req.session.user);
+          //save info the user_file 
+   const finalInfo = `${req.session.user}, logged Out at ${new Date().toISOString()}\n`; 
+          saveUserInfo(req.session.user,finalInfo)
+     console.log(`\x1b[1;36m${req.session.user} logged Out at ${new Date().toISOString()} \x1b[0m`)
+      req.session.destroy(()=>{
+          res.clearCookie("cypher.sid")
+   return res.redirect("/cyphers/login")
+})
+  
+    }catch(err){
+        saveErrorLog(req,err)
+      return res.status(500).send("An internal Error occured");
+ }
+  })
+}
+
+const User = (app)=>{
+  app.get("/cyphers/userId",(req,res)=>{
+    return res.status(200).send(req.session.user)
+ })
+}
+
+//password reset route
+
+const resetPassword = (app)=>{
+  app.patch("/cyphers/reset_password",async(req,res)=>{
+      try{
+    let { username, oldPassword, newPassword } = req.body;
+if(!req.session.user){
+  return sendError(res,401,`Oops, we could not verify ${ req.ip || "you" } as the rightful owner to this account, please try again later`)
+}
+    if(!username || !oldPassword || !newPassword){
+      return sendError(res,400,"all fields required");
+    }
+    const getUser = usernameResetSelect.get(username)
+    if(!getUser){
+   return sendError(res,404,"wrong username or username not found")
+}
+      const userOldPassword = getUser.signup_password;
+      const oldPasswordComparation = await bcrypt.compare(oldPassword, userOldPassword);
+     if(!oldPasswordComparation){
+   return sendError(res,400,"old password is wrong, please retry")
+}
+      const hashNewPassword = await bcrypt.hash(newPassword,10);
+      
+      const newPasswordUpdate = userPasswordReset.run(hashNewPassword,username)
+      const finalInfo = `A User Updated a password, identifier ${username} at ${new Date().toISOString()}\n`;
+
+ saveUserInfo(username,finalInfo)
+      
+      return sendSuccess(res,200,"password reset successfully");  
+      }catch(err){
+     return res.status(500).send("Oops an error occured please try afain later")
+          saveErrorLog(err)
+}
+  });
+  
+};
+
 
 
 export {
@@ -296,5 +591,14 @@ export {
     logDir,
     GitHub,
     totalRequests,
-    About
+    About,
+    signUpPOST,
+    signUpGET,
+    logOut,
+    date,
+    logInPOST,
+    logInGET,
+    User,
+    resetPassword
+          
 };
